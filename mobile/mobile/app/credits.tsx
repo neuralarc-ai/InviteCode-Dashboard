35a35c9e2e2cdee0a4db9fa0a0762f3aa315744d
yogi <yogi@Admins-MacBook-Pro.local>
2025-12-01 10:25:46 +0530

    Delete mobile app files: package files and app screens

--- mobile/mobile/app/credits.tsx
+++
@@ -1,7 +0,0 @@
-import { type ReactElement } from 'react';
-import { CreditBalanceTable } from '@/components/credit-balance-table';
-
-export default function CreditsScreen(): ReactElement {
-  return <CreditBalanceTable />;
-}
-
